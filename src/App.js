bd093b4c21cdecda30aa92a42c430f325392812b
Ilya Leykin <imleykin@gmail.com>
2019-02-07 17:01:22 +0500

    initial project structure

--- src/App.js
+++ src/App.js
@@ -3,26 +3,16 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+	render() {
+		return (
+			<div className="App">
+				<header className="App-header">
+					<h1 className="App-title">Мой топ фото</h1>
+				</header>
+				<p className="App-intro">Здесь будут мои самые залайканые фото</p>
+			</div>
+		);
+	}
 }
 
 export default App;
