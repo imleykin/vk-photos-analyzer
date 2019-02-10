8e61c4144c0d7f3377b8466866c0e3a100d338bd
Ilya Leykin <imleykin@gmail.com>
2019-02-10 12:41:09 +0500

    update App

--- src/App.js
+++ src/App.js
@@ -1,18 +1,31 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { connect } from 'react-redux';
 import './App.css';
 
 class App extends Component {
 	render() {
+		const { user, page } = this.props;
 		return (
 			<div className="App">
 				<header className="App-header">
 					<h1 className="App-title">Мой топ фото</h1>
 				</header>
+				<p>Привет, {user.name}</p>
+				<p>
+					У тебя {page.photos.length} фото за {page.year} год.
+				</p>
 				<p className="App-intro">Здесь будут мои самые залайканые фото</p>
 			</div>
 		);
 	}
 }
 
-export default App;
+const mapStateToProps = store => {
+	console.log(store);
+	return {
+		user: store.user,
+		page: store.page,
+	};
+};
+
+export default connect(mapStateToProps)(App);
