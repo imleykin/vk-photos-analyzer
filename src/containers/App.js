2a69d570dceed35a8295b87c807cae7eb65e7003
Ilya Leykin <imleykin@gmail.com>
2019-07-15 23:07:51 +0500

    update App layout

--- src/containers/App.js
+++ src/containers/App.js
@@ -2,15 +2,27 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import Page from '../components/Page';
 import User from '../components/User';
-import { setYear } from '../actions/PageActions';
+import { getPhotos } from '../actions/PageActions';
+import { handleLogin } from '../actions/UserActions';
 
 class App extends Component {
 	render() {
-		const { user, page, setYear } = this.props;
+		const { user, page, getPhotos, handleLogin } = this.props;
 		return (
 			<div className="app">
-				<Page photos={page.photos} year={page.year} setYear={setYear} />
-				<User name={user.name} />
+				<Page
+					photos={page.photos}
+					year={page.year}
+					error={page.error}
+					isFetching={page.isFetching}
+					getPhotos={getPhotos}
+				/>
+				<User
+					name={user.name}
+					isFetching={user.isFetching}
+					error={user.error}
+					handleLogin={handleLogin}
+				/>
 			</div>
 		);
 	}
@@ -22,7 +34,8 @@ const mapStateToProps = store => ({
 });
 
 const mapDispatchToProps = dispatch => ({
-	setYear: year => dispatch(setYear(year)),
+	getPhotos: year => dispatch(getPhotos(year)),
+	handleLogin: () => dispatch(handleLogin()),
 });
 
 export default connect(
