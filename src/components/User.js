9a6e89f2cc208706c3f993e9b9ad6d56697b7104
Ilya Leykin <imleykin@gmail.com>
2019-07-15 23:07:25 +0500

    update user component

--- src/components/User.js
+++ src/components/User.js
@@ -2,18 +2,38 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
 class User extends Component {
-	render() {
-		const { name } = this.props;
+	renderTemplate = () => {
+		const { name, error, isFetching } = this.props;
+
+		if (error) {
+			return <p>Во время запроса произошла ошибка, обновите страницу</p>;
+		}
+
+		if (isFetching) {
+			return <p>Загружаю...</p>;
+		}
+
+		if (name) {
+			return <p>Привет, {name}!</p>;
+		}
+
 		return (
-			<div className="ib user">
-				<p>Привет, {name}!</p>
-			</div>
+			<button className="btn" onClick={this.props.handleLogin}>
+				Войти
+			</button>
 		);
+	};
+
+	render() {
+		return <div className="ib user">{this.renderTemplate()}</div>;
 	}
 }
 
-User.propType = {
-	name: PropTypes.number.isRequired,
+User.propTypes = {
+	name: PropTypes.string.isRequired,
+	error: PropTypes.string,
+	isFetching: PropTypes.bool.isRequired,
+	handleLogin: PropTypes.func.isRequired,
 };
 
 export default User;
--- src/reducers/user.js
+++ src/reducers/user.js
@@ -1,5 +1,24 @@
+import {
+	LOGIN_REQUEST,
+	LOGIN_SUCCESS,
+	LOGIN_FAIL,
+} from '../actions/UserActions';
+
 const initialState = {
-	name: 'Аноним',
+	name: '',
+	error: '',
+	isFetching: false,
 };
 
-export default (state = initialState) => state;
+export default (state = initialState, action) => {
+	switch (action.type) {
+		case LOGIN_REQUEST:
+			return { ...state, isFetching: true, error: '' };
+		case LOGIN_SUCCESS:
+			return { ...state, isFetching: false, name: action.payload };
+		case LOGIN_FAIL:
+			return { ...state, isFetching: false, error: action.payload.message };
+		default:
+			return state;
+	}
+};
